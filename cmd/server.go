e77ec959135763b41e399cce73bd3a877326497d
Pedr0Rocha <pedrovqrocha@gmail.com>
2024-05-08 11:58:25 +0200

    server randomly returns error statuses

--- cmd/server.go
+++ cmd/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log/slog"
+	"math/rand"
 	"net/http"
 	"sync/atomic"
 )
@@ -12,6 +13,20 @@ func main() {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		requests.Add(1)
 		slog.Info("got request")
+
+		randomResp := rand.Intn(10) + 1
+
+		if randomResp <= 2 {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		if randomResp == 3 {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("hello"))
 	})
 
